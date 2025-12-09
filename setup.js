dd83ea99841898ab1d106460b0274bc081ea406a
MD-KAIF16 <mdkaif.1608@gmail.com>
2025-12-09 04:00:38 +0000

    Added setup.js

---
+++ setup.js
@@ -0,0 +1,7 @@
+console.log("1. Create GitHub account on github.com");
+console.log("2. Create a public repository");
+console.log("3. Open Codespace for browser coding");
+console.log("4. Check Node.js using node -v");
+console.log("5. Create JS files using VS Code");
+console.log("6. Run JS files using: node filename.js");
+console.log("7. Commit and push using git commands");
